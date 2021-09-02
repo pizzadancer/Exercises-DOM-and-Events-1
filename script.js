dbe64420022ea18a13b7b88e4c51050ff153faa1
dlm <dannyleematthew@gmail.com>
2021-09-02 01:52:51 -0400

    Shortened mouseover function

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ function init () {
     function liftOff() {
         document.getElementById("statusReport").innerHTML = "Houston, we have liftoff!";
     }
-    function redButton() {
-        document.getElementById("abortMission").style.backgroundColor = "red";
-    }
+   
     function clearButton() {
         document.getElementById("abortMission").style.backgroundColor = "";
     }
     button.addEventListener("click", liftOff);
-    missionAbort.addEventListener("mouseover", redButton);
+    missionAbort.addEventListener("mouseover", function() {
+        this.style.backgroundColor = "red";
+    });
     missionAbort.addEventListener("mouseleave", clearButton);
+    missionAbort.addEventListener("click", function (event) {
+        let ele = window.confirm("Are you sure you want to abort the mission?");
+        // console.log(ele)
+        if (ele) {
+            document.getElementById("statusReport").innerHTML = "Mission aborted! Space shuttle returning home.";
+        }
+    })
 }
 
 window.addEventListener("load", init);
